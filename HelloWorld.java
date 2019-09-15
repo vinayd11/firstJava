6647d545e79f90353c51ae93a386216931f496ed
vinayd11 <53546209+vinayd11@users.noreply.github.com>
2019-09-15 11:32:19 +0530

    asdf

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World FROM HIT automatic");
+    System.out.println("Hello, World FROM HIT automatic 2");
   }
 }
